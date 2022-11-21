2556106a43a23fd9c2244f0ab93565f1e25b431d
Julia-5534 <5534@holbertonstudents.com>
2022-11-20 16:48:25 -0800

    Some updates

---
+++ get_op.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * get_op - identifies which function corresponds to the string input
+ * @s: char input to match valid opcode
+ * Return: pointer to matched function, or NULL
+ */
+
+void (*get_op(char *s))(stack_t **stack, unsigned int line_num)
+{
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	unsigned int i = 0;
+
+	if (!s)
+		return (NULL);
+	while (ops[i].opcode != NULL && strcmp(s, ops[i].opcode) != 0)
+		i++;
+
+	return (ops[i].f);
+}
